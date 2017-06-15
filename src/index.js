e2951cb2152e1da226aa441d9851a72f1d40bea0
DrGonzo9 <dr.gonzo809@gmail.com>
2017-06-15 13:26:24 +0300

    New_3

--- src/index.js
+++ src/index.js
@@ -188,64 +188,64 @@ class Menu extends React.Component{
   render(){
     return(
       <div className="menu">
-                  <div className="menu_item">
-                    <img className="tool" src="img/tool.png" alt=""/>
-                    <img className="menu_img" src="img/home_icon.png" alt=""/>
-                    <span className="item_name">Моя Страница</span>
-                  </div>
-                  <div className="menu_item">
-                    <img className="tool" src="img/tool.png" alt=""/>
-                    <img className="menu_img" src="img/news.png" alt=""/>
-                    <span className="item_name">Новости</span>
-                  </div>
-                  <div className="menu_item">
-                    <img className="tool" src="img/tool.png" alt=""/>
-                    <img className="menu_img" src="img/messages.png" alt=""/>
-                    <span className="item_name">Сообщения</span>
-                  </div>
-                  <div className="menu_item">
-                    <img className="tool" src="img/tool.png" alt=""/>
-                    <img className="menu_img" src="img/homies.png" alt=""/>
-                    <span className="item_name">Друзья</span>
-                  </div>
-                  <div className="menu_item">
-                    <img className="tool" src="img/tool.png" alt=""/>
-                    <img className="menu_img" src="img/groups.png" alt=""/>
-                    <span className="item_name">Группы</span>
-                  </div>
-                  <div className="menu_item">
-                    <img className="tool" src="img/tool.png" alt=""/>
-                    <img className="menu_img" src="img/photo.png" alt=""/>
-                    <span className="item_name">Фотографии</span>
-                  </div>
-                  <div className="menu_item">
-                    <img className="tool" src="img/tool.png" alt=""/>
-                    <img className="menu_img" src="img/musique.png" alt=""/>
-                    <span className="item_name">Музыка</span>
-                  </div>
-                  <div className="menu_item">
-                    <img className="tool" src="img/tool.png" alt=""/>
-                    <img className="menu_img" src="img/movies.png" alt=""/>
-                    <span className="item_name">Видео</span>
-                  </div>
-                  <div className="menu_item">
-                    <img className="tool" src="img/tool.png" alt=""/>
-                    <img className="menu_img" src="img/games.png" alt=""/>
-                    <span className="item_name">Игры</span>
-                  </div>
-                  <div className="menu_item_split"></div>
-                  <div className="menu_item">
-                    <img className="tool" src="img/tool.png" alt=""/>
-                    <img className="menu_img" src="img/goodies.png" alt=""/>
-                    <span className="item_name">Товары</span>
-                  </div>
-                  <div className="menu_item">
-                    <img className="tool" src="img/tool.png" alt=""/>
-                    <img className="menu_img" src="img/docs.png" alt=""/>
-                    <span className="item_name">Документы</span>
-                  </div>
-          <Support />
+        <div className="menu_item">
+        <img className="tool" src="img/tool.png" alt=""/>
+        <img className="menu_img" src="img/home_icon.png" alt=""/>
+        <span className="item_name">Моя Страница</span>
         </div>
+        <div className="menu_item">
+          <img className="tool" src="img/tool.png" alt=""/>
+          <img className="menu_img" src="img/news.png" alt=""/>
+          <span className="item_name">Новости</span>
+        </div>
+        <div className="menu_item">
+          <img className="tool" src="img/tool.png" alt=""/>
+          <img className="menu_img" src="img/messages.png" alt=""/>
+          <span className="item_name">Сообщения</span>
+        </div>
+        <div className="menu_item">
+          <img className="tool" src="img/tool.png" alt=""/>
+          <img className="menu_img" src="img/homies.png" alt=""/>
+          <span className="item_name">Друзья</span>
+        </div>
+        <div className="menu_item">
+          <img className="tool" src="img/tool.png" alt=""/>
+          <img className="menu_img" src="img/groups.png" alt=""/>
+          <span className="item_name">Группы</span>
+        </div>
+        <div className="menu_item">
+          <img className="tool" src="img/tool.png" alt=""/>
+          <img className="menu_img" src="img/photo.png" alt=""/>
+          <span className="item_name">Фотографии</span>
+        </div>
+        <div className="menu_item">
+          <img className="tool" src="img/tool.png" alt=""/>
+          <img className="menu_img" src="img/musique.png" alt=""/>
+          <span className="item_name">Музыка</span>
+        </div>
+        <div className="menu_item">
+          <img className="tool" src="img/tool.png" alt=""/>
+          <img className="menu_img" src="img/movies.png" alt=""/>
+          <span className="item_name">Видео</span>
+        </div>
+        <div className="menu_item">
+          <img className="tool" src="img/tool.png" alt=""/>
+          <img className="menu_img" src="img/games.png" alt=""/>
+          <span className="item_name">Игры</span>
+        </div>
+        <div className="menu_item_split"></div>
+        <div className="menu_item">
+          <img className="tool" src="img/tool.png" alt=""/>
+          <img className="menu_img" src="img/goodies.png" alt=""/>
+          <span className="item_name">Товары</span>
+        </div>
+        <div className="menu_item">
+          <img className="tool" src="img/tool.png" alt=""/>
+          <img className="menu_img" src="img/docs.png" alt=""/>
+          <span className="item_name">Документы</span>
+        </div>
+        <Support />
+      </div>
     );
   }
 }
